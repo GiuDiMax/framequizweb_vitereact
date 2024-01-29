4249e52e2d728b5766a7e79a182e4d79bcd3173b
GiuDiMax <giuseppedimauro95@gmail.com>
2024-01-29 12:50:04 +0100

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  //const [count, setCount] = useState(0)
-    const [data, setData] = useState(null);
+    const [data, setData] = useState(0);
 
      function getTmdb() {
          fetch('https://api.themoviedb.org/3/discover/movie?api_key=03209f50e8253cf416b595048aced2ff')
@@ -17,17 +16,17 @@ function App() {
 
   return (
     <>
+      <div>
+          {data ? <img src={"https://image.tmdb.org/t/p/w1280" + data['backdrop_path']} alt="backdrop" className="frame"/> : ""}
+          <p>{data ? JSON.stringify(data, null, 2) : "Loading..."}</p>
+      </div>
 
-      <div>{data ? <pre>
-          <img src={"https://image.tmdb.org/t/p/w1280" + data['backdrop_path']} alt="backdrop" className="frame"/>
-          <p>{JSON.stringify(data, null, 2)}</p>
-      </pre> : 'Loading...'}</div>
-        <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => getTmdb()}>
           Click here
         </button>
       </div>
+
     </>
   )
 }
